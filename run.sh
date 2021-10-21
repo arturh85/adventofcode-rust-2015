db565bd08e4aca5ecfcae0e03db882ebd4032504
Artur Hallmann <artur.hallmann@tui.de>
2021-10-21 23:17:41 +0200

    added timeout

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ sudo sh -c 'echo -1 >/proc/sys/kernel/perf_event_paranoid'
 for (( i = 1; i <= 24; i++ )); do
   echo "Day $i"
   cargo aoc -d $i >> timings.txt
-  timeout -k 4m 3m cargo aoc flamegraph -d $i > /dev/null 2>&1
+  time timeout -k 6m 5m cargo aoc flamegraph -d $i > /dev/null 2>&1
   if test -f "target/aoc/aoc-autobench/flamegraph.svg"; then
     mv "target/aoc/aoc-autobench/flamegraph.svg" "flamegraph$i.svg"
   fi
