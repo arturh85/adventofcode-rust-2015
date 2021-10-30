76b9d7163765dc5dedab3821df2824b492b09ed0
Artur Hallmann <artur.hallmann@tui.de>
2021-10-30 22:57:27 +0200

    renamed days

--- .github/run.sh
+++ .github/run.sh
@@ -11,7 +11,7 @@ function aoc () {
       echo "- [Problem](https://adventofcode.com/2015/day/$1)" >> times.md
       echo "- [Solution](https://github.com/arturh85/adventofcode-rust-2015/blob/master/src/day$1.rs)" >> times.md
     fi
-    echo "### Part $2" >> times.md
+    echo "## Part $2" >> times.md
     cat times-$1-$2.md >> times.md
     echo "Generating flamegraph for Day $1 Part $2"
     time timeout -k 6m 5m cargo aoc flamegraph -d $1 -p $2 > /dev/null 2>&1
@@ -24,7 +24,7 @@ function aoc () {
 }
 
 echo "# Execution times for Advent of Code 2015" >> times.md
-echo "- 🗃️ [Github Repository](https://github.com/arturh85/adventofcode-rust-2015)" >> times.md
+echo "- 🔖 [Github Repository](https://github.com/arturh85/adventofcode-rust-2015)" >> times.md
 echo "- 🚀 Benchmarked using [Github Actions](https://github.com/features/actions)" >> times.md
 for (( i = 1; i <= 24; i++ )); do
   aoc $i 1
