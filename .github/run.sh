f5514d2cc93dede6371502af4945ef515d7322eb
Artur Hallmann <artur.hallmann@tui.de>
2021-10-30 21:45:06 +0200

    better times.md

--- .github/run.sh
+++ .github/run.sh
@@ -6,8 +6,9 @@ function aoc () {
   echo "Running Day $1 Part $2"
   cargo aoc -d $1 -p $2 | grep -v "AOC 2015" | sed 's/,/ /g' | sed 's/generator/'"\\${nl}"'- generator/g' | sed 's/runner/'"\\${nl}"'- runner/g' > times-$1-$2.md
   if test -s times-$1-$2.md; then
-    echo "## 2015 Day $1 Part $2" >> times.md
+    echo "## Day $1 Part $2" >> times.md
     cat times-$1-$2.md >> times.md
+    echo "Generating flamegraph for Day $1 Part $2"
     time timeout -k 6m 5m cargo aoc flamegraph -d $1 -p $2 > /dev/null 2>&1
     if test -f "target/aoc/aoc-autobench/flamegraph.svg"; then
       mv "target/aoc/aoc-autobench/flamegraph.svg" "flamegraph$1-$2.svg"
@@ -17,7 +18,8 @@ function aoc () {
   fi
 }
 
-echo "# Github Action Execution times for Advent of Code 2015" >> times.md
+echo "# Execution times for Advent of Code 2015" >> times.md
+echo "🚀 Run with [Github Actions](https://github.com/features/actions)" >> times.md
 for (( i = 1; i <= 24; i++ )); do
   aoc $i 1
   aoc $i 2
