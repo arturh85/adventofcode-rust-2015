5c2dbfd00d0ed28981dc4f2f4cb8274eff4ed6ce
Artur Hallmann <artur.hallmann@tui.de>
2021-10-30 21:19:55 +0200

    better times.md

--- .github/run.sh
+++ .github/run.sh
@@ -2,10 +2,11 @@
 sudo sh -c 'echo -1 >/proc/sys/kernel/perf_event_paranoid'
 
 function aoc () {
+  nl=$'\n'
   echo "Running Day $1 Part $2"
-  cargo aoc -d $1 -p $2 | grep -v "AOC 2015" | sed 's/generator/- generator/g' | sed 's/runner/- runner/g' > times-$1-$2.md
+  cargo aoc -d $1 -p $2 | grep -v "AOC 2015" | sed 's/generator/'"\\${nl}"'- generator/g' | sed 's/runner/- runner/g' > times-$1-$2.md
   if test -s times-$1-$2.md; then
-    echo "### Part $2" >> times.md
+    echo "## 2015 Day $1 Part $2" >> times.md
     cat times-$1-$2.md >> times.md
     time timeout -k 6m 5m cargo aoc flamegraph -d $1 -p $2 > /dev/null 2>&1
     if test -f "target/aoc/aoc-autobench/flamegraph.svg"; then
@@ -18,7 +19,6 @@ function aoc () {
 
 echo "# Github Action Execution times for Advent of Code 2015" >> times.md
 for (( i = 1; i <= 24; i++ )); do
-  echo "## 2015 Day $i" >> times.md
   aoc $i 1
   aoc $i 2
 done
