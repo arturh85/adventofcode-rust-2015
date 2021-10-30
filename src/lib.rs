718e0252fc694e5bc3b063c298cf90966a1d69e2
Artur Hallmann <artur.hallmann@tui.de>
2021-10-30 22:49:59 +0200

    renamed days

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,13 @@
 extern crate aoc_runner_derive;
 extern crate aoc_runner;
 
-mod day01;
-mod day02;
-mod day03;
-mod day04;
-mod day05;
-mod day06;
-mod day07;
-// mod day08;
+mod day1;
+mod day2;
+mod day3;
+mod day4;
+mod day5;
+mod day6;
+mod day7;
+// mod day8;
 
 aoc_lib! { year = 2015 }
