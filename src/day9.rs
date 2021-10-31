b0f27bf26762e3fbeccc511246cff6df00e045d3
Artur Hallmann <artur.hallmann@tui.de>
2021-10-31 18:58:30 +0100

    split generator for day9 + completed tests

--- src/day9.rs
+++ src/day9.rs
@@ -47,73 +47,75 @@
 use itertools::Itertools;
 use std::collections::HashMap;
 
+#[aoc_generator(day9)]
+fn parse_input_day9(input: &str) -> anyhow::Result<HashMap<String, HashMap<String, u64>>> {
+    parse_routes(input)
+}
+
+type RoutingMap = HashMap<String, HashMap<String, u64>>;
+
 /// What is the distance of the shortest route?
 #[aoc(day9, part1)]
-fn part1(input: &str) -> u64 {
-    let connections = parse_connections(input);
-    shortest_connection(connections)
+fn part1(routes: &RoutingMap) -> u64 {
+    *lengths(routes)
+        .iter()
+        .min()
+        .expect("routes should not be empty")
 }
 
 /// What is the distance of the longest route?
 #[aoc(day9, part2)]
-fn part2(input: &str) -> u64 {
-    let connections = parse_connections(input);
-    longest_connection(connections)
+fn part2(routes: &RoutingMap) -> u64 {
+    *lengths(routes)
+        .iter()
+        .max()
+        .expect("routes should not be empty")
 }
 
-fn shortest_connection(connections: HashMap<String, HashMap<String, u64>>) -> u64 {
-    let mut shortest = u64::MAX;
-    for perm in connections.keys().permutations(connections.keys().len()) {
-        let mut len = 0;
-        for flight in perm.windows(2) {
-            let source = flight[0];
-            let target = flight[1];
-            len += connections[source][target];
-        }
-        if len < shortest {
-            shortest = len;
-        }
-    }
-    shortest
+fn lengths(routes: &RoutingMap) -> Vec<u64> {
+    routes
+        .keys()
+        .permutations(routes.keys().len())
+        .map(|perm| {
+            let mut len = 0;
+            for flight in perm.windows(2) {
+                let source = flight[0];
+                let target = flight[1];
+                len += routes[source][target];
+            }
+            len
+        })
+        .collect()
 }
 
-fn longest_connection(connections: HashMap<String, HashMap<String, u64>>) -> u64 {
-    let mut longest = 0;
-    for perm in connections.keys().permutations(connections.keys().len()) {
-        let mut len = 0;
-        for flight in perm.windows(2) {
-            let source = flight[0];
-            let target = flight[1];
-            len += connections[source][target];
-        }
-        if len > longest {
-            longest = len;
-        }
-    }
-    longest
-}
-
-fn parse_connections(input: &str) -> HashMap<String, HashMap<String, u64>> {
-    let mut m = HashMap::new();
+fn parse_routes(input: &str) -> anyhow::Result<RoutingMap> {
+    let mut routes = HashMap::new();
     for line in input.lines() {
         let vec: Vec<&str> = line.split(" ").collect();
+        if vec.len() != 5 {
+            return Err(anyhow!("invalid format, expected 4 words per line"));
+        }
+        // London to Dublin = 464
+        // 0      1  2      3 4
         let source = vec[0];
         let target = vec[2];
         let distance = vec[4];
-        if !m.contains_key(source) {
-            m.insert(source.to_string(), HashMap::new());
+        if !routes.contains_key(source) {
+            routes.insert(source.to_string(), HashMap::new());
         }
-        if !m.contains_key(target) {
-            m.insert(target.to_string(), HashMap::new());
+        if !routes.contains_key(target) {
+            routes.insert(target.to_string(), HashMap::new());
         }
-        m.get_mut(source)
+        routes
+            .get_mut(source)
             .unwrap()
             .insert(target.to_string(), distance.parse().unwrap());
-        m.get_mut(target)
+        routes
+            .get_mut(target)
             .unwrap()
             .insert(source.to_string(), distance.parse().unwrap());
     }
-    m
+    Ok(routes)
 }
 
 #[cfg(test)]
@@ -122,13 +124,29 @@ mod tests {
 
     #[test]
     fn part1_examples() {
-        let input = "London to Dublin = 464
+        let routes = parse_routes(
+            "London to Dublin = 464
 London to Belfast = 518
-Dublin to Belfast = 141";
-        let map = parse_connections(input);
-        assert_eq!(shortest_connection(map), 605);
+Dublin to Belfast = 141",
+        )
+        .expect("failed to parse example");
+
+        // The shortest of these is `London -> Dublin -> Belfast = 605`, and so
+        // the answer is `605` in this example.
+        assert_eq!(part1(&routes), 605);
     }
 
     #[test]
-    fn part2_examples() {}
+    fn part2_examples() {
+        let routes = parse_routes(
+            "London to Dublin = 464
+London to Belfast = 518
+Dublin to Belfast = 141",
+        )
+        .expect("failed to parse example");
+
+        // For example, given the distances above, the longest route would be `982`
+        // via (for example) `Dublin -> London -> Belfast`.
+        assert_eq!(part2(&routes), 982);
+    }
 }
